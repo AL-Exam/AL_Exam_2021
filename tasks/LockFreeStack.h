5fcf63916380faf7729a8ec329c8ef66041daed1
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2022-01-08 17:08:44 +0300

    Update LockFreeStack.h

--- tasks/LockFreeStack.h
+++ tasks/LockFreeStack.h
@@ -15,18 +15,17 @@ class LockFreeStack
     std::atomic<Node<T>*> head;
  public:
  
-    void Push(const T& data) {
-        
-        // std::memory_order_relaxed не накладывает никаких ограничений,
+    // std::memory_order_relaxed не накладывает никаких ограничений,
         // необходима для гарантирования атомарности операции
       
         // std::memory_order_release необходима для того, чтобы
         // запись и чтение в текущем потоке не могли быть переупорядочены,
         // и другие потоки могли видеть данную атомарную переменную
       
-        // compare_exchange_weak побитово сравнивает lhs и rhs и выполняется
-        // только когда они побитово равны
-        
+        // compare_exchange_weak побитово сравнивает lhs и rhs и меняет
+        // lhs на rhs только когда они побитово равны
+    
+    void Push(const T& data) {
         node<T>* new_node = new node<T>(data);
         new_node->next = head.load(std::memory_order_relaxed); 
         while(!head.compare_exchange_weak(new_node->next, new_node,
@@ -35,7 +34,11 @@ class LockFreeStack
     }
     
     bool TryPop(T& value) {
-        
+        Node* old_head = head.load(std::memory_order_relaxed);
+        while (old.head && !head.compare_exchange_weak (old_head, old_head -> next,
+                                                        std::memory_order_relaese,
+                                                        std::memory_order_relaxed));
+        return old_head ? true : false;
     }
 
 };
